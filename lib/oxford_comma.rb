e6bba55b8b174b9e6dc79eb299462d194622f8bc
Daiane Bushey <daianebushey@gmail.com>
2017-12-29 21:56:19 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+  if array.length === 1
+    #turn the array into a str
+    array.join
+  elsif array.length === 2
+    #str1 "and" str2
+    array.join(" and ")
+  elsif array.length === 3
+    #str1 "," str2 "," "and" str3
+    first = array.pop
+    last = array.slice(2)
 
+  else
+    #turn the array into str, all elements separeted by "," "and" last element
+    array*", "
+  end
 end
